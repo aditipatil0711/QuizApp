f5a78380398e2af9886099f0154190ba2eaeed36
Aditi Patil <patiladiti752@gmail.com>
2025-01-23 12:41:24 -0800

    Colors for correctness

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -50,6 +50,10 @@ startGame = () => {
 
 getNewQuestion = () => {
 
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        return window.location.assign('/end.html');
+    }
+
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
@@ -60,5 +64,34 @@ getNewQuestion = () => {
         const number = choice.dataset['number'];
         choice.innerText = currentQuestion['choice' + number];
     });
-}
+
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+
+};
+
+choices.forEach(choice => {
+
+    choice.addEventListener('click', e => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+        console.log(classToApply);
+
+        selectedChoice.parentElement.classList.add(classToApply);
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+
+
+        console.log(selectedAnswer == currentQuestion.answer);
+       
+
+    });
+});
 startGame();
